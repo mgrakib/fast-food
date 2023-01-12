946b0c8c70eac4c9dd885b8f3f3f7ff4e94ab475
mgrakib <mgrakibbd@gmail.com>
2023-01-12 20:56:30 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+// ------------- hambar subsection ----------
+
 const hambarSection = document.getElementById("hambarSection");
 
+const hambarSubmenuItemsH = document.getElementById("hambarSubmenuItemsH");
+const hambarSubmenuItemsM = document.getElementById("hambarSubmenuItemsM");
+const hambarSubmenuItemsS = document.getElementById("hambarSubmenuItemsS");
+const hambarSubmenuItemsP = document.getElementById("hambarSubmenuItemsP");
+const hambarSubmenuItemsB = document.getElementById("hambarSubmenuItemsB");
 
 function hambarClose() {
     hambarSection.style.left = "-100%";
@@ -9,4 +16,23 @@ function hambarClose() {
 function hambarOpen() {
     hambarSection.style.left = "0";
     hambarSection.style.opacity = "1";
+}
+
+
+// ------------- hambar subsection ----------
+
+function showHamsubH() {
+    hambarSubmenuItemsH.style.display = "block";
+}
+function showHamsubM() {
+    hambarSubmenuItemsM.style.display = "block";
+}
+function showHamsubS() {
+    hambarSubmenuItemsS.style.display = "block";
+}
+function showHamsubP() {
+    hambarSubmenuItemsP.style.display = "block";
+}
+function showHamsubB() {
+    hambarSubmenuItemsB.style.display = "block";
 }
